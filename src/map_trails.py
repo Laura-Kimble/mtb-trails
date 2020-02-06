62b4effc1ac08502ee8dc804f23907425e15b630
Laura Kimble <laura@Geoffreys-MacBook-Pro.local>
2020-02-06 11:59:42 -0700

    folium maps

---
+++ src/map_trails.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import datetime
+import folium
+from st_sklearn_nmf import get_topic_trails
+
+def create_multiple_dots_layers(df, base_map, layer_names, trail_names_for_layers):
+    ''' Create layers on a folium map.
+
+    ARGS:
+        df (dataframe): Dataframe with all data to be filtered for each layer, with 'lat' and 'lon' columns
+        base_map (folium Map object): Map to apply the layers to
+        layer_names (list of str): Names for the layers.
+        trail_names_for_layers (list of list of tuples): Each element in the list is a list of tuples with the 'top trails' for a topic.
+            The first element in the tuple is the trail name, and the second is the region (not used in this mapping function).
+    '''
+
+    colors = ['red', 'orange', 'blue', 'purple', 'green', 'black']
+
+    for idx, layer in enumerate(layer_names):
+        trail_names = [trail for trail, region in trail_names_for_layers[idx]]
+        layer_df = df[df['name'].map(lambda x: x in trail_names)]  
+        color = colors[idx % 7]
+        create_dots_layer(layer_df, base_map, layer, color=color)
+        
+
+def create_dots_layer(df, base_map, layer_name, color='black'):
+    '''
+    Create a dots layer onto the base map, with the lat/lon in the dataframe.
+  
+    ARGS:
+        df (dataframe): Data to show in the layer, with 'lat' and 'lon' columns
+        base_map (folium Map object): Base map to apply the layer to
+        layer_name (str): Name of the layer.
+        color (str): Color of the dots.
+    '''
+
+    feature_map = folium.FeatureGroup(name = layer_name)
+
+    for idx, row in df.iterrows():
+        folium.CircleMarker(location=(row['lat'], row['lon']),
+                                    radius=1.5,
+                                    color=color,
+                                    popup=str(row['name'] \
+                                              + '\n\nDifficulty: '+ str(row['difficulty']) \
+                                             ),
+                                    fill=True).add_to(feature_map)
+
+    base_map.add_child(feature_map)
+
+
+if __name__ == '__main__':
+
+    #Load the pickeled dataframes
+    st_df = pd.read_pickle('../data/st_trails_df_2')
+    W_df = pd.read_pickle('../models/W_df')
+
+    top_topic_trails = get_topic_trails(W_df.values, n=50)
+    topic_names = W_df.columns
+
+    base_map = folium.Map(location=[39.73782,-98],
+                            zoom_start=5,
+                            tiles="Cartodbpositron")
+
+
+    # Create map layers
+    create_multiple_dots_layers(st_df, base_map, topic_names, top_topic_trails)
+
+    # add toggle controls for the layers
+    folium.LayerControl().add_to(base_map)
+
+    base_map.save('../images/base_map.html')
--- src/st_sklearn_nmf.py
+++ src/st_sklearn_nmf.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import joblib
 from sklearn.decomposition import NMF
 from gensim.parsing.preprocessing import STOPWORDS
 from mtb_sklearn_lda import make_vectorizer, transform_vectorizer, get_top_words
@@ -19,25 +20,33 @@ def get_st_regions():
     return st_df_with_desc['region']
 
 
-def print_top_topic_words(H, vectorizer_model, n=10):
-    feature_names = np.array(vectorizer_model.get_feature_names())
+def print_top_topic_words(H, feature_names, n=10):
     for i, topic in enumerate(H):
         top_topic_words = feature_names[topic.argsort()[:-n-1:-1]]
         print(f'Topic {i}:')
         print(top_topic_words)
 
 
-def print_topic_trails(W, vectorizer_model, n=10):
+def print_topic_trails(trail_list):
+    for i, trails in enumerate(trail_list):
+        print(f'Topic {i}:')
+        for trail, region in trails:
+            print(f'{trail}: {region}')
+        print('\n')
+
+
+def get_topic_trails(W, n=10):
+    all_topic_trails = []
     trail_names = get_st_trail_names()
     regions = get_st_regions()
+    
     for i, topic in enumerate(W.T):
         top_ind = topic.argsort()[:-n-1:-1]
         top_trails = trail_names[top_ind]
-        top_regions = regions[top_ind]
-        print(f'Topic {i}:')
-        for trail, region in zip(top_trails, top_regions):
-            print(f'{trail}: {region}')
-        print('\n')
+        top_regions = regions[top_ind] 
+        all_topic_trails.append(list(zip(top_trails, top_regions)))
+    
+    return all_topic_trails
 
 
 def fit_nmf(mat, n_topics, alpha=0.):
@@ -55,7 +64,7 @@ if __name__ == '__main__':
 
     # Create  set of stopwords to use 
     my_stopwords = set(['climb', 'mountain', 'road', 'singletrack', 'loop', 'trail', 'trails',  'ride', 'area', 'route', 'way', \
-                        'feature', 'section','sections', 'riding', \
+                        'feature', 'section','sections', 'riding', 'll', 'rd', 'going', 'stay', \
                     'loop','br', 'mile', 'miles', 'right', 'left', 'www', 'http', 'https', 'bike', 'bikes', 'bicycle', 'bicycles', \
                     'continue', 'rider', 'riders', 'parking', 'lot', 'turn', 'start', 'starts', 'description', 'cross', \
                     'north', 'south', 'east', 'west', '-PRON-', 'pron', 'nee', 'regard', 'shall', 'use', 'win', \
@@ -69,10 +78,31 @@ if __name__ == '__main__':
     tfidf_vect = make_vectorizer(X, tf_idf=True, lemmatize=False, max_df=0.8, min_df=2, max_features=1000, stop_words=all_stopwords, ngram_range=(1, 2))
     tfidf = transform_vectorizer(tfidf_vect, X)
     # top_words = get_top_words(tfidf_vect, tfidf, n=50)
+    
+    feature_names = np.array(tfidf_vect.get_feature_names())
+    trail_names = get_st_trail_names()
 
     # NMF
     k = 6
+    topics = ['topic_{}'.format(i) for i in range(k)]
+
     nmf, W, H = fit_nmf(tfidf, n_topics=k, alpha=0.)
+
+    print(f'reconstruction error: {nmf.reconstruction_err_:0.3}')
+
+    print_top_topic_words(H, feature_names, n=10)
+    print('\n\n')
+    all_topic_trails = get_topic_trails(W, n=10)
+    print_topic_trails(all_topic_trails)
+
+    # Dump vectorizer, model and W and H as dataframes
+    joblib.dump(nmf, '../models/nmf_model.joblib')
+    joblib.dump(tfidf_vect, '../models/tfidf_vec.joblib')
+
+    W_df = pd.DataFrame(W, index=trail_names, columns=topics)
+    H_df = pd.DataFrame(H, index=topics, columns=feature_names)
+    W_df.to_pickle('../models/W_df')
+    H_df.to_pickle('../models/H_df')
 
     # Plot reconstruction error for different num topics
     # k_list = np.arange(2, 15, 2)
@@ -84,11 +114,4 @@ if __name__ == '__main__':
     # fig, ax = plt.subplots(figsize=(10, 6))
     # ax.plot(k_list, errors)
     # plt.show()
-
-
-    print(f'reconstruction error: {nmf.reconstruction_err_:0.3}')
-
-    print_top_topic_words(H, tfidf_vect, n=10)
-    print('\n\n')
-    print_topic_trails(W, tfidf_vect, n=10)
 
