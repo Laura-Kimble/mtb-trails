120ed0ce7aed5251e21391d05e32051df90a9611
Laura-Kimble <Laura.E.Detwiler@gmail.com>
2020-02-26 11:00:42 -0700

    web app work

--- web_app/app.py
+++ web_app/app.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd 
 import joblib
 import sys
 sys.path.insert(1, '../src')
-import item_recommender as ir
-import importlib
-importlib.reload(ir)
+# import item_recommender as ir
+# import importlib
+# importlib.reload(ir)
 
 app = Flask(__name__)
 
 recommender = joblib.load('../models/trail_recommender.joblib')
+df = pd.read_pickle('../data/co_trails_df')
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-# @app.route('/inputs', methods=['GET'])
-# def inputs():
-#     return render_template('inputs.html')
+@app.route('/inputs', methods=['GET'])
+def inputs():
+    return render_template('inputs.html', df=df)
 
 # @app.route('/recommendations', methods=['POST'])
 # def recommendations():
@@ -27,5 +29,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
 
