117babf4f4971cfa5bc3566dc7d35f6e0db111f4
Laura-Kimble <Laura.E.Detwiler@gmail.com>
2020-02-26 12:45:06 -0700

    web app updates

--- web_app/app.py
+++ web_app/app.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 recommender = joblib.load('../models/trail_recommender.joblib')
 df = pd.read_pickle('../data/co_trails_df')
 
+def shorten_text_col(df, colname, num_chars=50):
+    df[colname] = df[colname].map(lambda x: x[:num_chars]+'...' if len(x)>num_chars else x)
+    return df
+
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -20,12 +25,19 @@ def index():
 def inputs():
     return render_template('inputs.html', df=df)
 
-# @app.route('/recommendations', methods=['POST'])
-# def recommendations():
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+    trail = request.form['trail']
+    if trail=='':
+        return 'You must select a trail.'
 
-#     data = str(request.form['article_body'])
-#     pred = str(model.predict([data])[0])
-#     return render_template('predict.html', article=data, predicted=pred)
+    display_cols = ['name', 'region_name', 'difficulty', 'length', 'rating', 'description']
+    recco_names, recco_ids = recommender.get_recommendations(trail, n=5)
+    trail_df = df[df['name']==trail][display_cols]
+    trail_df = shorten_text_col(trail_df, 'description', num_chars=200)
+    reccos_df = df[df['id'].map(lambda x: x in recco_ids)][display_cols]
+    reccos_df = shorten_text_col(reccos_df, 'description', num_chars=200)
+    return render_template('recommendations.html', trail_df=trail_df, reccos_df=reccos_df)
 
 
 if __name__ == '__main__':
